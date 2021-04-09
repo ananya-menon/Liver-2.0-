101a60a264133b152737ffb3cbc56a44b5f14494
Ananya M Menon <60038609+ananya-menon@users.noreply.github.com>
2021-04-09 21:55:33 +0530

    Update pcos.py

--- pcos.py
+++ pcos.py
@@ -287,33 +287,3 @@ import pickle
  filename = 'liver.pkl' 
  pickle.dump(classifier_forest, open(filename, 'wb'))
 
-from flask import Flask, request, render_template
-import pickle
-import numpy as np
-
-app = Flask(__name__)
-model = pickle.load(open('liver.pkl', 'rb')) 
-
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/predict', methods = ['GET', 'POST'])
-def predict():
-
-    int_features = [x for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    print(prediction)
-    output = prediction[0]
-    
-    if output == 0:
-        return render_template('home.html', prediction_text= 'PCOS : No')
-    else:
-        return render_template('home.html', prediction_text= 'PCOS : Yes') 
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
